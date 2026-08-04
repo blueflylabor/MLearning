cf4214571fa12d2a507e91c8e61a946bc0b83712
藍翔技工 <48398805+blueflylabor@users.noreply.github.com>
2026-08-04 23:11:33 +0800

    Initial Commit

---
+++ MLearning/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  MLearning
+//
+//  Created by Yancey Yang on 8/4/26.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return EXIT_SUCCESS;
+}
